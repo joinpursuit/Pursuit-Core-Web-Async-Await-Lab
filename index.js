54410de154c01083757b07475a160ab843951cb7
Miguel Garcia <miguelgarcia@pursuit.org>
2021-03-16 21:49:48 -0400

    functional but not passing

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-const button= document.querySelector("#draw-cards-bttn")
+const button = document.querySelector("#draw-cards-bttn");
+let deckData;
+
+
 const deckOfcards = async () => {
-    try {
-     const response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-      let deckId = response.data.deck_id
-      let remaining = response.data.remaining
-      let response2= await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`)
-      let cards = response2.data.cards
-      console.log(cards)
-      cards.forEach((card) =>{
-          const img= document.createElement("img")
-          img.classList.add(".card")
-          img.src = card.image
-          const section = document.querySelector("#card-image")
-          section.appendChild(img)
-          const p= document.querySelector("#remaining")
-          p.textContent = remaining
+  try {
+    const response = await axios.get(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    );
+    deckData = response.data;
+    console.log(deckData);
+  } catch (err) {
+    console.log("The error was thrown", err);
+  }
+};
 
 
-        }) 
-    } catch (err) {
-        console.log("The error that was thrown: ", err)
-    }
+
+const drawCards = async () => {
+  try {
+    let deckId = deckData.deck_id;
+    let response2 = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`
+    );
+    let cards = response2.data.cards;
+    let remaining = response2.data.remaining;
+    console.log(cards);
+    cards.forEach((card) => {
+      const img = document.createElement("img");
+      img.classList.add(".card");
+      img.src = card.image;
+      const section = document.querySelector("#card-image");
+      section.appendChild(img);
+      const p = document.querySelector("#remaining");
+      p.textContent = `Remaining Cards:  ${Number(remaining)}`;
+    });
+} catch (err) {
+    console.log("The error that was thrown: ", err);
 }
-deckOfcards();
+};
 
-button.addEventListener("click", e=>{
-    e.preventDefault()
-    deckOfcards()
-})
+
+
+
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    drawCards();
+    // let remaining = deckData.remaining
+    // const p = document.querySelector("#remaining");
+});
+
+
+deckOfcards();
