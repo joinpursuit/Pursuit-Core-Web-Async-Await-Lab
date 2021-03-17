7f62007c156cc5a2b57aab3201bc907f8b2a4665
Obaid Rustemi <orustemi@gmail.com>
2021-03-17 10:59:40 -0400

    cards remaining works now

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
 // const { default: axios } = require("axios");
 
 const select = document.querySelector("select");
-const cardUL = document.querySelector("ul")
-const form = document.querySelector("form")
+const cardUL = document.querySelector("ul");
+const form = document.querySelector("form");
 const cardSubmitButton = document.querySelector("#cardButton");
 const remaining = document.querySelector("#remaining");
+const cardDisplaySection = document.querySelector("#card-display-section");
 
 const cards = (num) => {
-    for (let i = 1; i <= num; i++) {
-        const option = document.createElement("option");
-        option.textContent = i;
-        option.value = i;
-        select.appendChild(option);
-    }
+  for (let i = 1; i <= num; i++) {
+    const option = document.createElement("option");
+    option.textContent = i;
+    option.value = i;
+    select.appendChild(option);
+  }
 };
 cards(10);
-// select.value = "5"
-//   debugger;
+
 
 form.addEventListener("submit", async (e) => {
-e.preventDefault();
+  e.preventDefault();
+
   const numOfCards = Number(select.value);
-
-  const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
-
+  const res = await axios.get(
+    `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
+  );
   const deckID = res.data.deck_id;
-  const cardsRemaining = `${res.data.remaining} card(s) left `;
-
-  const res2 = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${numOfCards}`);
-
+  const res2 = await axios.get(
+    `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${numOfCards}`
+  );
+  const cardsRemaining = `${res2.data.remaining} card(s) left `;
   const cards = res2.data.cards;
-  
-  const cardDisplaySection = document.querySelector("#card-display-section");
   cardUL.textContent = "";
+
   
   cards.forEach((card) => {
     const cardImg = document.createElement("img");
     cardImg.src = card.image;
     cardImg.classList.add("card");
     const li = document.createElement("li");
-
-    cardUL.appendChild(li)
+    
+    cardUL.appendChild(li);
     li.appendChild(cardImg);
-  
-    // remaining.appendChild(cardsRemaining);
-  });
 });
 
-// create event listener for the submit button
-// load cards
-// itterate through and display # of cards that user selects
-//
+remaining.innerHTML = cardsRemaining;
+
+});
+
+
