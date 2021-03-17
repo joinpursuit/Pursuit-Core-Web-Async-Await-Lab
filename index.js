bca1eb7d5ded72fb8c0795cd7333f95e99691c84
joshuamendez <joshuafmendez@gmail.com>
2021-03-16 22:00:52 -0400

    select calls card bug

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const createOptions = (num) => {
         option.value = i;
         select.appendChild(option);
     }
-    // document.querySelector('option[value][value="5"]').selected = true;
+    document.querySelector('option[value][value="5"]').selected = true;
 }
 createOptions(10);
-// console.log(selectedOption)
-// selectedOption.selected = true;
 const button = document.querySelector('#get-cards-button');
 const remainingCardsDisplay = document.querySelector('#remaining-cards');
 const getDeck = async (e) => {
@@ -23,21 +21,20 @@ const getDeck = async (e) => {
         let remainingCards = drawCard.data.remaining;
         console.log(drawCard.data);
         const deckLength = drawCard.data.cards.length;
-        button.addEventListener('click', () => {
-            cardDisplay.innerHTML = '';
-            for(let i = 0; i < deckLength; i++){
-                const cardImg = document.createElement('img');
-                cardImg.classList.add('.card');
-                cardImg.src = drawCard.data.cards[i].image;
-                cardDisplay.appendChild(cardImg);
-                // console.log(drawCard.data.cards[i]);
-                remainingCardsDisplay.textContent = `${remainingCards} card(s) left`;
-            }
-        });
         console.log(remainingCards);
+        cardDisplay.innerHTML = '';
+        for(let i = 0; i < deckLength; i++){
+            const cardImg = document.createElement('img');
+            cardImg.classList.add('.card');
+            cardImg.src = drawCard.data.cards[i].image;    
+            cardDisplay.appendChild(cardImg);
+            remainingCardsDisplay.textContent = `${remainingCards} card(s) left`;
+        }
     } catch (err) {
         console.log(err);
     }
 }
+const getCard = () => {
+};
 select.addEventListener('change', getDeck);
-// button.addEventListener('click', () => {console.log('help')});
+button.addEventListener('click', getCard);
