5f293346d5411de7b9bdc935034fbc0599e2f712
Yashiry Azize <azizeyashiry@gmail.com>
2021-03-16 21:56:05 -0400

    in progress

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const select = document.querySelector("select");
+const section = document.querySelector("#card-display")
 
+const cardsDrawn = (num) => {
+  for (let i = 1; i <= num; i++) {
+    const option = document.createElement("option");
+    option.textContent = i;
+    option.value = i;
+    select.appendChild(option);
+  }
+};
+
+cardsDrawn(10);
+const getCards = async (e) => {
+    const numOfCards = Number(e.target.value)
+    try {
+    const deckCount = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+    const deckId = deckCount.data.deck.id;
+    const drawCard = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=5`);
+    const remainingCards = drawCard.data.remaining;
+    for (let i =0; i <= )
+    console.log(remainingCards);
+    console.log(drawCard.data);
+
+}
+}
+
+
+button.addEventListener("click", async (event) => {
+   
+})
