421cc8e8aabe5a8b4d106a5eb51a4be2b271e1cd
vrwatson <vanessawatson@pursuit.org>
2021-03-16 22:30:17 -0400

    fetching new deck & default 5 cards

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const select = document.querySelector("select");
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 const p = document.querySelector("p");
+let deckID = "";
 
 const createOptions = () => {
   for (i = 1; i <= 10; i++) {
     const option = document.createElement("option");
+    if(i === 5) {
+        option.selected = true;
+    }
     option.textContent = i;
     //option.value = i;  if we use the form button this assignment of the value separates the value from the event to get the #
     select.appendChild(option);
@@ -13,25 +17,67 @@ const createOptions = () => {
 };
 createOptions(10);
 
+const newDeck = async () => {
+  try {
+    const res = await axios.get("https://deckofcardsapi.com/api/deck/new/");
+    deckID += res.data.deck_id;
+    console.log(`res = ${res}`)
+    console.log(`deckID = ${deckID}`)
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+window.addEventListener("load", (event) => {
+    newDeck();
+    // debugger
+});
+
+// const firstDeal = async () => {
+//   try {
+//     const res = await axios.get(
+//       `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=5`
+//     );
+//     res.data.cards.forEach((card) => {
+//         const li = document.createElement("li");
+//         li.innerHTML = "";
+//         ul.appendChild(li);
+//         const img = document.createElement("img");
+//         img.src = card.image;
+//         li.appendChild(img);
+//       });
+//     p.textContent = `${res.data.remaining} card(s) left`;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// firstDeal();
+
+const dealCards = async () => {
+  debugger;
+  try {
+    const res = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${select.value}`
+    );
+    debugger;
+    res.data.cards.forEach((card) => {
+      const li = document.createElement("li");
+      li.innerHTML = "";
+      ul.appendChild(li);
+      const img = document.createElement("img");
+      img.classList.add("card");
+      img.src = card.image;
+      li.appendChild(img);
+    });
+    p.textContent = `${res.data.remaining} card(s) left`;
+  } catch (err) {
+    console.log(err);
+  }
+};
+// dealCards();
+
 form.addEventListener("submit", (event) => {
-event.preventDefault()
-    // const loadedDeck = () => { 
-    ul.textContent = ""        
-        fetch (`https://deckofcardsapi.com/api/deck/new/draw/?count=${select.value}`)
-        .then (res =>{
-            return res.json()
-        }).then (res =>{
-            res.cards.forEach((card) => {
-                const li = document.createElement("li");
-                li.innerHTML = "" 
-                ul.appendChild(li);
-                const img = document.createElement("img"); 
-                img.src = card.image
-                li.appendChild(img)
-                
-            })
-            p.textContent = `${res.remaining} card(s) left`
-            // return res.message
-        })
-    })
-// })
+  event.preventDefault();
+  ul.textContent = "";
+  dealCards();
+});
