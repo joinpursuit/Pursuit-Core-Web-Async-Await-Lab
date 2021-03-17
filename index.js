a9686f1cff7c1940fcf4adb11fd7ba22cda43479
Durdona Djalilova <ddjalilova3@gmail.com>
2021-03-16 22:02:38 -0400

    halfway

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 const selectBar = document.querySelector("#select-bar") 
 const button = document.querySelector("button")
-const createOptions = (num) => {
-  for (let i = 1; i <= num; i++) {
-    const option = document.createElement("option")
-    option.textContent = i;
-    option.value = i;
-    selectBar.appendChild(option)
-  }
-}
+let deckId
+let remaining
 
-createOptions(10);
+
+// const createOptions = (num) => {
+//   for (let i = 1; i <= num; i++) {
+//     const option = document.createElement("option")
+//     option.textContent = i;
+//     option.value = i;
+//     selectBar.appendChild(option)
+//   }
+// }
+
+// createOptions(10);
 
 const shuffleCards = async () =>{
-    const section = document.querySelector("section")
     try {
-        const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
-        const deckId = res.data.deck_id;
-        const remaining = res.data.remaining;
+        const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=5`)
+        debugger
+         deckId = res.data.deck_id;
+         remaining = res.data.remaining;
     }
  catch (err) {
     console.log(err)
 }
+}
+
+
 shuffleCards()
 
+
+
 button.addEventListener("click", async (e) => {
-    try{
-  const numOfCards = Number(e.target.value);
-  const count = 0
+  try{
+    let numOfCards = Number(selectBar.value);
+    
   const res1 = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numOfCards}`)
-  debugger
+  
     const cardsSection = document.querySelector("section");
     cardsSection.innerHTML = ""
+
     res1.data.cards.forEach(card => {
       const img = document.createElement("img");
-      img.src = cards[i].image;
-      cards.appendChild(img);
+      img.src = card.image;
+      card.classlist.add("card")
+      cardsSection.appendChild(img);
   })
-  .catch(err => {
+} catch (err) {
     console.log(err);
   }
 })
+
