f3a8dbf84e84014a17fa82ad0ea6e3a581558342
Daliz Cruz <dalizcruz@gmail.com>
2021-03-16 20:40:55 -0400

    lab problem 1

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+const selectBar = document.querySelector("#select-bar") 
+const button = document.querySelector("button")
+const createOptions = (num) => {
+  for (let i = 1; i <= num; i++) {
+    const option = document.createElement("option")
+    option.textContent = i;
+    option.value = i;
+    selectBar.appendChild(option)
+  }
+}
 
+createOptions(10);
+
+const shuffleCards = async () =>{
+    const section = document.querySelector("section")
+    try {
+        const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+        const deckId = res.data.deck_id;
+        const remaining = res.data.remaining;
+    }
+ catch (err) {
+    console.log(err)
+}
+shuffleCards()
+
+button.addEventListener("click", async (e) => {
+    try{
+  const numOfCards = Number(e.target.value);
+  const count = 0
+  const res1 = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numOfCards}`)
+  debugger
+    const cardsSection = document.querySelector("section");
+    cardsSection.innerHTML = ""
+    res1.data.cards.forEach(card => {
+      const img = document.createElement("img");
+      img.src = cards[i].image;
+      cards.appendChild(img);
+  })
+  .catch(err => {
+    console.log(err);
+  }
+})
