e01e73aa635db7e55179eaaf3e83c789375f3af6
Shoaib Dar <SDAR30@gmail.com>
2021-03-16 21:31:51 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-
-
 document.addEventListener("DOMContentLoaded", async (e) => {
-	const deck = await axios.get("https://deckofcardsapi.com/api/deck/new/");
+	const deck = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
 	const deckId = deck.data.deck_id;
 
 
-	const shuffle = async (deckId) => {
-		const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
-	}
-	shuffle(deckId);
+	// const shuffle = async (deckId) => {
+	// 	const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
+	// }
+	// shuffle(deckId);
 
 	const button = document.querySelector("#btn");
 	button.addEventListener("click", async (e) => {
@@ -20,13 +18,13 @@ document.addEventListener("DOMContentLoaded", async (e) => {
 		const cards = await axios.get(
 			`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${num}`
 		);
-		p.textContent = `${cards.data.remaining} card(s) left`
+		p.textContent = `${cards.data.remaining} card(s) left.`
 		debugger
 		cardPictures.innerHTML = "";
 		cards.data.cards.forEach((card) => {
 			const img = document.createElement("img");
 			img.src = card.image;
-			img.className = "card";
+			img.classList.add("card");
 			cardPictures.appendChild(img);
 		});
 
