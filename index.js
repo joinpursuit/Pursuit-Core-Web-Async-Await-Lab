213201fe305829f6e79551255ac1280ec2b86a7d
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-03-17 21:13:26 -0400

    finish lab and pass specs

--- index.js
+++ index.js
@@ -3,6 +3,17 @@
 // - Displays those cards to the user.
 // - When the user clicks the button again, draws another set of cards from that same deck.
 // - Shows the user how many cards remain in the deck, and updates this number every time cards are drawn.
+let apiId = "";
+const originalGet = async () => {
+  try {
+    let newDeck = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+    apiId += newDeck.data.deck_id;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", originalGet);
 
 const select = document.querySelector("#select-cards");
 
@@ -13,6 +24,7 @@ const createOptions = () => {
     select.appendChild(option);
     if (option.textContent === "5") {
       option.id = "five";
+      select.value = "5";
       document.getElementById("five").selected = "true";
     }
   }
@@ -21,23 +33,25 @@ createOptions();
 
 const getCards = async (event) => {
   let numCards = select.value;
+
   try {
-    const res1 = await axios.get(`https://deckofcardsapi.com/api/deck/new/`);
-    const resId = res1.data.deck_id;
-    const res2 = await axios.get(
-      `https://deckofcardsapi.com/api/deck/${resId}/draw/?count=${numCards}`
+    const res = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${apiId}/draw/?count=${numCards}`
     );
 
+    debugger;
     const displayCards = document.querySelector("#display-cards");
+    displayCards.innerHTML = "";
     const numOfCards = res.data.cards;
     numOfCards.forEach((el) => {
       const img = document.createElement("img");
-      img.src = el.image;
+      img.classList.add("card");
+      img.src = el.images.png;
       displayCards.appendChild(img);
     });
 
     const remaining = document.getElementById("remaining");
-    remaining.textContent = `${res.data.remaining} card(s) remaining`;
+    remaining.textContent = `${res.data.remaining} card(s) left.`;
   } catch (err) {
     console.log(err);
   }
@@ -45,3 +59,5 @@ const getCards = async (event) => {
 
 let cardsButton = document.querySelector("#cards-button");
 cardsButton.addEventListener("click", getCards);
+
+document.addEventListener("DOMcontentLoaded", originalGet);
