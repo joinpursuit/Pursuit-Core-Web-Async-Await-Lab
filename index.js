ef4f382b08a2ff2f03c3fd96d3e16fff11306003
Jordan Bobadilla <jordanboba08@gmail.com>
2021-03-18 02:47:27 -0400

    all minor bugs fixed

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 const form = document.querySelector("form");
 const select = document.querySelector("select");
 const p = document.querySelector("#remaining")
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const section = document.querySelector("section");
-  const getDeck = async (e) => {
-    try {
-      const res = await axios.get(
-        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+const section = document.querySelector("section");
+
+let deckId
+const getDeck = async (e) => {
+  try {
+    const res = await axios.get(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
       );
-      const deckId = res.data.deck_id;
+      deckId = res.data.deck_id;
+      debugger
+  } catch (error) {
+    console.log(error);
+  }
+};
+getDeck();
+
+form.addEventListener("submit", async (e) => {
+  try {
+      e.preventDefault();
       const res2 = await axios.get(
         `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${Number(
           select.value
         )}`
       );
       section.textContent = "";
-      p.textContent = ""
+      const remaining = res2.data.remaining 
+      p.textContent = `${remaining} card(s) left.`
       res2.data.cards.forEach((image) => {
         const img = document.createElement("img");
         img.classList.add("card")
         img.src = image.image;
         section.appendChild(img);
+        // remaining--
       });
-      const remaining = res2.data.remaining 
-      p.textContent = `${remaining} card(s) left.`
+      debugger
     } catch (error) {
       console.log(error);
     }
-  };
-  getDeck();
 });
