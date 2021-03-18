8e5519c3b58f4bc61547a8c7352fd61b53a6a5f1
cassidybeni <cassidybeni@pursuit.org>
2021-03-17 23:14:14 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let button = document.querySelector("button")
+    let div = document.querySelector("div")
+    let p = document.querySelector('#remaining')
 
+    const shuffle = async () => {
+        
+        let res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+        let deckId = res.data.deck_id
+
+        button.addEventListener("click", event => {
+            event.preventDefault()
+
+            let input = (document.querySelectorAll("option")[document.querySelector("select").selectedIndex].innerHTML)
+
+            const draw = async () => {
+                let res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${input}`)
+                let cards = res.data.cards
+                
+                div.innerHTML = ""
+                p.innerHTML = `${res.data.remaining} card(s) left.`
+
+                cards.forEach(el => {
+                    let img = document.createElement('img')
+                    img.classList.add("card")
+                    img.src = el.images.png
+                    div.appendChild(img)
+                })
+                
+            }
+            draw()
+
+        })
+
+    }
+    shuffle()
+})
