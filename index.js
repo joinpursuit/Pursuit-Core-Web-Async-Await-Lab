623c766fd1d90a5f3a8a633ff4e6a26cc47e02ed
Obaid Rustemi <orustemi@gmail.com>
2021-03-19 20:25:53 -0400

    all main tests passing

--- index.js
+++ index.js
@@ -12,37 +12,50 @@ const cards = (num) => {
     option.value = i;
     select.appendChild(option);
   }
+  select.value = "5";
 };
 cards(10);
+
+let deckId;
+
+const getNewDeck = async () => {
+  try {
+    const res = await axios.get(
+      `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
+    );
+    deckID = res.data.deck_id;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getNewDeck();
 
 form.addEventListener("submit", async (e) => {
   try {
     e.preventDefault();
 
-  const numOfCards = Number(select.value);
-  const res = await axios.get(
-    `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
-  );
-  const deckID = res.data.deck_id;
-  const res2 = await axios.get(
-    `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${numOfCards}`
-  );
-  const cardsRemaining = `${res2.data.remaining} card(s) left `;
-  const cards = res2.data.cards;
-  cardUL.textContent = "";
+    const numOfCards = Number(select.value);
+    
+    const res2 = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${numOfCards}`
+    );
+    const cardsRemaining = `${res2.data.remaining} card(s) left.`;
+    const cards = res2.data.cards;
+    cardUL.textContent = "";
 
-  cards.forEach((card) => {
-    const cardImg = document.createElement("img");
-    cardImg.src = card.image;
-    cardImg.classList.add("card");
-    const li = document.createElement("li");
+    cards.forEach((card) => {
+      const cardImg = document.createElement("img");
+      cardImg.src = card.image;
+      cardImg.classList.add("card");
+      const li = document.createElement("li");
 
-    cardUL.appendChild(li);
-    li.appendChild(cardImg);
-  });
+      cardUL.appendChild(li);
+      li.appendChild(cardImg);
+    });
 
-  remaining.innerHTML = cardsRemaining;
-} catch (err) {
-    remaining.innerHTML = `Sorry there was an error.`
-}
+    remaining.innerHTML = cardsRemaining;
+  } catch (err) {
+    remaining.innerHTML = `Sorry there was an error.`;
+  }
 });
