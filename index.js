a72c5e5ec58c9996e1cdd0e06fad58ebed4a974b
Jordan Alvarado <jordan.alvarado@me.com>
2021-03-21 22:27:26 -0400

    WIP (Jordan)

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+async function shuffleDeck() {
+    let result = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    console.log(result.data)
 
+    //deconstructing
+
+    //let deckID = result.data.deck_id
+    //let remaining = result.data.remaining
+
+    let {deck_id: deckID, remaining} = result.data
+    // console.log(deckID)
+    // console.log(remaining)
+    return {
+        deckID: deckID,
+        remaining: remaining
+    }
+}
+
+function drawCard() {
+
+}
+
+let deck = await shuffleDeck()
+console.log(deck)
