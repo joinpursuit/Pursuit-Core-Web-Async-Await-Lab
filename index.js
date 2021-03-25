7fd69d03ef0765ffaa6bb22cfff0ccf718330d26
Talia Pichardo <taliapichardo@gmail.com>
2021-03-25 15:53:17 -0400

    drawing cards

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const getData = async () =>{
+    const res = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+    const deck = res.data
+    console.log(deck)
+    let {deck_id, remaining} = deck
+    console.log(deck_id)
+    console.log(remaining)
+    
+    const drawCard = () =>{
+        
+    }
+}
+
+getData()
+
 
