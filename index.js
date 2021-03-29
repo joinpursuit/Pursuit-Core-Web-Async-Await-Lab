2ed9258b1feac9d3c14f4b0783f6cd2cbacd5645
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-03-28 20:31:56 -0400

    wow am i stupid i need to practice more

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 
+const cardContainer = document.querySelector('#card-container');
+let cardNum = document.querySelector('#remaining')
+const button = document.querySelector("button");
+let option = document.querySelector('select')
+
+
+
+
+async function cardGenerator(){
+  
+
+    try {
+        
+        const response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        const deck = response.data
+
+            let uniqueDeck = deck.deck_id
+            
+            
+
+               
+            
+            button.addEventListener("click", displayCards)
+            async function displayCards() { 
+                let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/${uniqueDeck}/draw/?count=${option.value}`)
+                let selectedCards = drawCards.data
+
+                cardContainer.innerHTML = ""
+
+                for (let card of selectedCards.cards) {
+                    let image = document.createElement("img")
+                    image.classList.add("card")
+                    image.src = card.image
+                    cardContainer.appendChild(image)
+                  
+                }
+                  cardNum.innerText = `${selectedCards.remaining} card(s) left.`
+            }
+        
+    } catch (err) { 
+
+        console.log(err)
+
+    }
+}
+
+cardGenerator()
